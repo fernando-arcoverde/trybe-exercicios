04855fccb163ddb1b38b06e7f89c3393f03789e1
fernando-arcoverde <nando.arcoverde@gmail.com>
2023-02-28 09:21:22 -0300

    Exercício 01

---
+++ fundamentos/secao-03-introducao-javascript/dia-01-javascript-primeiros-passos/dia-01-exercicios/script.js
@@ -0,0 +1,7 @@
+// Exercício 1
+
+let myName = 'Carolina';
+let birthCity = 'Recife';
+const birthYear = 1985;
+
+console.log('Olá, meu nome é ' + myName + ', sou de ' + birthCity + ' e nasci no ano de ' + birthYear + '.');
