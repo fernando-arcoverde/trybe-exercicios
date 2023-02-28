66aa35f3953b4ef8e82749229d9668e567705b14
fernando-arcoverde <nando.arcoverde@gmail.com>
2023-02-28 10:34:55 -0300

    Exercício 02

--- fundamentos/secao-03-introducao-javascript/dia-01-javascript-primeiros-passos/dia-01-exercicios/script.js
+++ fundamentos/secao-03-introducao-javascript/dia-01-javascript-primeiros-passos/dia-01-exercicios/script.js
@@ -5,3 +5,32 @@ let birthCity = 'Recife';
 const birthYear = 1985;
 
 console.log('Olá, meu nome é ' + myName + ', sou de ' + birthCity + ' e nasci no ano de ' + birthYear + '.');
+
+// Verificando tipos de variáveis.
+
+let patientId = 50;
+let isEnrolled = true;
+let patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+let patientEmail = 'ana@email.com';
+
+console.log(typeof patientId);
+console.log(typeof isEnrolled);
+console.log(typeof patientInfo);
+console.log(typeof patientEmail);
+
+patientId = "80";
+
+console.log(typeof patientId);
+
+// Exercício 02
+
+const base = 5;
+const height = 8;
+let area = base * height;
+let perimeter = 5 + 5 + 8 + 5;
+
+console.log(area);
+console.log(perimeter);
