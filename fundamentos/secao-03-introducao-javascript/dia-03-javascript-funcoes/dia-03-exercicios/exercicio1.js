a64468a51e743aaf216d6c967ec1210020d6d308
fernando-arcoverde <nando.arcoverde@gmail.com>
2023-03-01 13:37:22 -0300

    Exercício 01

---
+++ fundamentos/secao-03-introducao-javascript/dia-03-javascript-funcoes/dia-03-exercicios/exercicio1.js
@@ -0,0 +1,25 @@
+let saldo = 1345;
+
+function somaSaldo(valor) {
+    return saldo + valor;
+}
+
+function subtraiSaldo(valor) {
+    return saldo - valor;
+}
+
+function multiplicaSaldo(valor) {
+    return saldo * valor;
+}
+
+function divideSaldo(valor) {
+    return saldo / valor;
+}
+
+console.log(somaSaldo(1255)); // 2660
+
+console.log(subtraiSaldo(477)); // 868
+
+console.log(multiplicaSaldo(3)); // 4035
+
+console.log(divideSaldo(4)); // 336.25
