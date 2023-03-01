c3d5285dee83f5e1769a25c8cf48ae60a7757dd9
fernando-arcoverde <nando.arcoverde@gmail.com>
2023-03-01 10:49:30 -0300

    Sexto Requisito do Exercício Prático

--- fundamentos/secao-03-introducao-javascript/dia-02-javascript-array-e-loop-for/dia-02-exercicios/exercise2.js
+++ fundamentos/secao-03-introducao-javascript/dia-02-javascript-array-e-loop-for/dia-02-exercicios/exercise2.js
@@ -1,18 +1,11 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let sum = 0;
-let media = 0;
-let maxValue = numbers[0];
-let numImpares = 0;
-let mostrarNumerosImpares;
 
-for (let index = 0; index < numbers.length; index += 1) {
-    if (numbers[index] % 2 !== 0) {
-        numImpares += 1;
+let menorNumero = numbers[0];
+
+for (let index = 1; index < numbers.length; index += 1) {
+    if (numbers[index] < menorNumero) {
+        menorNumero = numbers[index];
     }
 }
 
-if (numImpares > 0) {
-    console.log('Existem ' + numImpares + ' números ímpares dentro do array.');
-} else {
-    console.log('Nenhum valor ímpar encontrado.');
-}
+console.log(menorNumero);
