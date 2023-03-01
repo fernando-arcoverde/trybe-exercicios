72de9940fe0952cb05bca60bba2f8fcaf89ab30f
fernando-arcoverde <nando.arcoverde@gmail.com>
2023-03-01 10:56:12 -0300

    Sétimo Requisito do Exercício Prático

--- fundamentos/secao-03-introducao-javascript/dia-02-javascript-array-e-loop-for/dia-02-exercicios/exercise2.js
+++ fundamentos/secao-03-introducao-javascript/dia-02-javascript-array-e-loop-for/dia-02-exercicios/exercise2.js
@@ -1,11 +1,8 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numbers = [];
 
-let menorNumero = numbers[0];
 
-for (let index = 1; index < numbers.length; index += 1) {
-    if (numbers[index] < menorNumero) {
-        menorNumero = numbers[index];
-    }
+for (let index = 1; index <= 25; index += 1) {
+    numbers.push(index);
 }
 
-console.log(menorNumero);
+console.log(numbers);
