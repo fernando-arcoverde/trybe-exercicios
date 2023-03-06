a8c4f7dfc7e5162c5f0fde377ebc5cf8d50c91bd
fernando-arcoverde <nando.arcoverde@gmail.com>
2023-03-06 10:03:40 -0300

    Sexto Requisito

--- fundamentos/secao-03-introducao-javascript/dia-01-javascript-primeiros-passos/dia-01-exercicios/pratica-exercicios-dia-01.js
+++ fundamentos/secao-03-introducao-javascript/dia-01-javascript-primeiros-passos/dia-01-exercicios/pratica-exercicios-dia-01.js
@@ -31,17 +31,45 @@
 //   console.log('zero');
 // }
 
-let angulaA = 30;
-let anguloB = 120;
-let anguloC = 30;
-let somaAngulos = angulaA + anguloB + anguloC;
+// let angulaA = 30;
+// let anguloB = 120;
+// let anguloC = 30;
+// let somaAngulos = angulaA + anguloB + anguloC;
 
-if(angulaA > 0 && anguloB > 0 && anguloC > 0) {
-    if(somaAngulos === 180) {
-        console.log(true);
-    } else {
-        console.log(false);
-    }
-} else {
-    console.log('Ângulo Inválido!');
+// if (angulaA > 0 && anguloB > 0 && anguloC > 0) {
+//     if (somaAngulos === 180) {
+//         console.log(true);
+//     } else {
+//         console.log(false);
+//     }
+// } else {
+//     console.log('Ângulo Inválido!');
+// }
+
+// ====================================
+
+let pecaXadrez = 'rei';
+
+switch (pecaXadrez) {
+    case 'rei':
+        console.log(pecaXadrez + ': Uma casa apenas para qualquer direção.');
+        break;
+    case 'rainha':
+        console.log(pecaXadrez + ': Se mova em qualquer direção quantas casas quiser.');
+        break;
+    case 'bispo':
+        console.log(pecaXadrez + ': Movimentos diagonais.');
+        break;
+    case 'cavalo':
+        console.log(pecaXadrez + ': Movimentos em "L" e pode pular sobre outras peças.');
+        break;
+    case 'torre':
+        console.log(pecaXadrez + ': Movimentos horizontais e verticais.');
+        break;
+    case 'peão':
+        console.log(pecaXadrez + ': No primeiro movimento pode movimentar duas casas para frente, após o primeiro movimento, apenas uma casa.');
+        break;
+
+    default:
+        console.log('Peça não encontrada.');
 }
