dcde2323abe1f4bf68b250d904d7b8a54aad82eb
fernando-arcoverde <nando.arcoverde@gmail.com>
2023-03-11 10:24:59 -0300

    Praticando for e for/of

--- fundamentos/secao-03-introducao-javascript/dia-02-javascript-array-e-loop-for/dia-02-exercicios/for.js
+++ fundamentos/secao-03-introducao-javascript/dia-02-javascript-array-e-loop-for/dia-02-exercicios/for.js
@@ -1,17 +1,33 @@
-let cars = ["Saab", "Volvo", "BMW"];
+// let cars = ["Saab", "Volvo", "BMW"];
 
-for (let index = 0; index < cars.length; index += 1) {
-    console.log(cars[index]);
+// for (let index = 0; index < cars.length; index += 1) {
+//     console.log(cars[index]);
+// }
+
+// let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+// for (index = 0; index < groceryList.length; index += 1) {
+//     console.log(groceryList[index]);
+// }
+
+// let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+// for (let imprimir of names) {
+//     console.log(imprimir);
+// }
+
+let tasksListDomingo = ['Acordar', 'Tomar Banho', 'Descer as escadas', 'Colocar Jack para limpar o chão', 'Limpar Jack'];
+
+// for (let index = 0; index < tasksListDomingo.length; index += 1) {
+//     console.log(tasksListDomingo[index]);
+// };
+
+for (let tasksList of tasksListDomingo) {
+    console.log(tasksList);
 }
 
-let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+const tasksListSabado = 'Estudar';
 
-for (index = 0; index < groceryList.length; index += 1) {
-    console.log(groceryList[index]);
-}
-
-let names = ['João', 'Maria', 'Antônio', 'Margarida'];
-
-for (let imprimir of names) {
-    console.log(imprimir);
+for (let estudar of tasksListSabado) {
+    console.log(estudar);
 }
