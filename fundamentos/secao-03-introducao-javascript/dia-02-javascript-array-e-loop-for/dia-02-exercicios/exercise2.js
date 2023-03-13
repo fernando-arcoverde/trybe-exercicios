da68fa94f6ac458ec2079f991899ab29ca7c5b2d
fernando-arcoverde <nando.arcoverde@gmail.com>
2023-03-13 10:26:13 -0300

    fazendo os requisitos dos exercícios dia 2

--- fundamentos/secao-03-introducao-javascript/dia-02-javascript-array-e-loop-for/dia-02-exercicios/exercise2.js
+++ fundamentos/secao-03-introducao-javascript/dia-02-javascript-array-e-loop-for/dia-02-exercicios/exercise2.js
@@ -1,8 +1,48 @@
-let word = 'tryber';
-let reverseWord = '';
+// let word = 'tryber';
+// let reverseWord = '';
 
-for (let index = 0; index < word.length; index += 1) {
-  reverseWord += word[word.length - 1 - index];
+// for (let index = 0; index < word.length; index += 1) {
+//   reverseWord += word[word.length - 1 - index];
+// }
+
+// console.log(reverseWord);
+
+// ===================================
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+let media = sum / numbers.length;
+
+for(let index = 0; index < numbers.length; index += 1) {
+  sum += numbers[index];
+};
+
+if (media > 20) {
+  console.log('Valor maior que 20');
+} else {
+  console.log('Valor menor ou igual a 20');
 }
 
-console.log(reverseWord);
+let maiorValor = numbers[0];
+
+for(let index = 1; index < numbers.length; index += 1) {
+  if (numbers[index] > maiorValor) {
+    maiorValor = numbers[index];
+  }
+}
+
+console.log(maiorValor);
+
+let oddNumber = 0;
+
+for(let index = 0; index < numbers.length; index += 1) {
+  if (numbers[index] % 2 !== 0) {
+    oddNumber += 1;
+  }
+}
+
+if (oddNumber === 0) {
+  console.log('Nenhum valor ímpar encontrado!');
+} else {
+  console.log(oddNumber);
+}
--- fundamentos/secao-03-introducao-javascript/dia-02-javascript-array-e-loop-for/dia-02-exercicios/for.js
+++ fundamentos/secao-03-introducao-javascript/dia-02-javascript-array-e-loop-for/dia-02-exercicios/for.js
@@ -1,26 +1,26 @@
-// let cars = ["Saab", "Volvo", "BMW"];
+let cars = ["Saab", "Volvo", "BMW"];
 
-// for (let index = 0; index < cars.length; index += 1) {
-//     console.log(cars[index]);
-// }
+for (let index = 0; index < cars.length; index += 1) {
+    console.log(cars[index]);
+}
 
-// let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
 
-// for (index = 0; index < groceryList.length; index += 1) {
-//     console.log(groceryList[index]);
-// }
+for (index = 0; index < groceryList.length; index += 1) {
+    console.log(groceryList[index]);
+}
 
-// let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
 
-// for (let imprimir of names) {
-//     console.log(imprimir);
-// }
+for (let imprimir of names) {
+    console.log(imprimir);
+}
 
 let tasksListDomingo = ['Acordar', 'Tomar Banho', 'Descer as escadas', 'Colocar Jack para limpar o chão', 'Limpar Jack'];
 
-// for (let index = 0; index < tasksListDomingo.length; index += 1) {
-//     console.log(tasksListDomingo[index]);
-// };
+for (let index = 0; index < tasksListDomingo.length; index += 1) {
+    console.log(tasksListDomingo[index]);
+};
 
 for (let tasksList of tasksListDomingo) {
     console.log(tasksList);
@@ -30,4 +30,11 @@ const tasksListSabado = 'Estudar';
 
 for (let estudar of tasksListSabado) {
     console.log(estudar);
+}
+
+const tabuada = [20, 20, 20, 20, 20];
+
+for (let contador of tabuada) {
+    contador += 20
+    console.log(contador);
 }
