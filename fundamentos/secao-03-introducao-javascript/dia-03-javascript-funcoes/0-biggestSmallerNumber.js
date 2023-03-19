46ef3cc8491e843ef60991d2f77f972e13c1db74
fernando-arcoverde <nando.arcoverde@gmail.com>
2023-03-19 13:52:38 -0300

    Treinando Funções - revisitando aula ao vivo

--- fundamentos/secao-03-introducao-javascript/dia-03-javascript-funcoes/0-biggestSmallerNumber.js
+++ fundamentos/secao-03-introducao-javascript/dia-03-javascript-funcoes/0-biggestSmallerNumber.js
@@ -1,4 +1,24 @@
 
+// function biggestSmallerNumber (array) {
+//   let bigger = array[0];
+//   let smaller = array[0];
+
+//   for (let index = 1; index < array.length; index += 1) {
+//     if (array[index] > bigger) {
+//       bigger = array[index];
+//     } else if (array[index] < smaller) {
+//       smaller = array[index];
+//     }
+//   }
+//   return `Maior: ${bigger}
+// Menor: ${smaller}`;
+// };
+
+// console.log(biggestSmallerNumber([-19, 7, 2, 3, 1, 6, 9, 4, 5]));
+// console.log(biggestSmallerNumber([19, 56, 789]));
+
+// ==================================
+
 function biggestSmallerNumber (array) {
   let bigger = array[0];
   let smaller = array[0];
@@ -10,9 +30,7 @@ function biggestSmallerNumber (array) {
       smaller = array[index];
     }
   }
-  return `Maior: ${bigger}
-Menor: ${smaller}`;
-};
+  return `Maior: ${bigger} e Menor: ${smaller}`;
+}
 
-console.log(biggestSmallerNumber([-19, 7, 2, 3, 1, 6, 9, 4, 5]));
-console.log(biggestSmallerNumber([19, 56, 789]));
+console.log(biggestSmallerNumber([7, 2, 3, 1, 6, 9, 4, 5, 16, 72, 0]));
