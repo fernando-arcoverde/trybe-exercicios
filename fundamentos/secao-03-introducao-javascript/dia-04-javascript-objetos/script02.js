15820545d9ea7ee22d7ed0c6572e2e894f64c576
fernando-arcoverde <nando.arcoverde@gmail.com>
2023-03-20 09:46:08 -0300

    fixando for/in e for/of

---
+++ fundamentos/secao-03-introducao-javascript/dia-04-javascript-objetos/script02.js
@@ -0,0 +1,25 @@
+let cars = ['Saab', 'Volvo', 'BMW'];
+
+for (let index in cars) {
+  console.log(cars[index]);
+}
+
+// ===========================
+
+let car = {
+    type: 'Fiat',
+    model: '500',
+    color: 'white',
+  };
+  
+  for (let index in car) {
+    console.log(index, car[index]);
+  }
+
+  // ===========================
+
+  let food = ['hambúrguer', 'bife', 'acarajé'];
+
+  for (let index of food) {
+    console.log(index);
+  }
