2bf0fca891ae577ceee958096049abd41c0f870e
fernando-arcoverde <nando.arcoverde@gmail.com>
2023-03-21 15:33:45 -0300

    iniciando exercício 03 - bônus

---
+++ fundamentos/secao-03-introducao-javascript/dia-04-javascript-objetos/dia-04-exercicios/exercicio03-bonus.js
@@ -0,0 +1,19 @@
+let lesson1 = {
+    materia: 'Matemática',
+    numeroEstudantes: 20,
+    professor: 'Maria Clara',
+    turno: 'manhã',
+  };
+  
+  let lesson2 = {
+    materia: 'História',
+    numeroEstudantes: 20,
+    professor: 'Carlos',
+  };
+  
+  let lesson3 = {
+    materia: 'Matemática',
+    numeroEstudantes: 10,
+    professor: 'Maria Clara',
+    turno: 'noite',
+  };
