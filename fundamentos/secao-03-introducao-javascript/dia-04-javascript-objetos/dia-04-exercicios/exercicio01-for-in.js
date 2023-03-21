9fe046c5be33e3afa41c2d33a070dd59ebd59cbf
fernando-arcoverde <nando.arcoverde@gmail.com>
2023-03-21 14:47:35 -0300

    exercicio 1 - requisito 6

--- fundamentos/secao-03-introducao-javascript/dia-04-javascript-objetos/dia-04-exercicios/exercicio01-for-in.js
+++ fundamentos/secao-03-introducao-javascript/dia-04-javascript-objetos/dia-04-exercicios/exercicio01-for-in.js
@@ -1,25 +1,25 @@
-let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  };
+// let info = {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   };
 
-  let info2 = {
-    personagem: 'Tio Patinhas',
-    origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
-    nota: 'O último MacPatinhas',
-    recorrente: 'Sim',
-  };
+//   let info2 = {
+//     personagem: 'Tio Patinhas',
+//     origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
+//     nota: 'O último MacPatinhas',
+//     recorrente: 'Sim',
+//   };
 
-  info.recorrente = 'Sim';
+//   info.recorrente = 'Sim';
 
-  for (let index in info) {
-    if (index === 'recorrente' && info[index] === 'Sim' && info2[index] === 'Sim') {
-        console.log('Ambos recorrentes');
-    } else {
-        console.log(info[index] + ' e ' + info2[index]);
-    }
-  }
+//   for (let index in info) {
+//     if (index === 'recorrente' && info[index] === 'Sim' && info2[index] === 'Sim') {
+//         console.log('Ambos recorrentes');
+//     } else {
+//         console.log(info[index] + ' e ' + info2[index]);
+//     }
+//   }
 
 //   console.log(`Bem-vinda, ${info.personagem}!`);
 
@@ -35,3 +35,31 @@ let info = {
 //     console.log(info[index]);
 //   }
 
+// =================
+
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+  };
+
+  console.log(leitor.nome);
+  console.log(leitor.sobrenome);
+  console.log(leitor.livrosFavoritos[0].titulo);
+
+  console.log(`O livro favorito de ${leitor.nome} ${leitor.sobrenome} se chama ${leitor.livrosFavoritos[0].titulo}.`);
+
+  leitor.livrosFavoritos.push({
+    titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+    autor: 'JK Rowling',
+    editora: 'Rocco',
+  })
+
+  console.log(leitor.livrosFavoritos);
