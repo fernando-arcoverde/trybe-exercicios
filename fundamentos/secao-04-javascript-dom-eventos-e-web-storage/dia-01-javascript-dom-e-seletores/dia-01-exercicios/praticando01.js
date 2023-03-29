7316658d6a099fe8b33b05021d917c94cb87aa6f
fernando-arcoverde <nando.arcoverde@gmail.com>
2023-03-29 11:16:47 -0300

    Secao-04-DOM - Praticando DOM-01

---
+++ fundamentos/secao-04-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/dia-01-exercicios/praticando01.js
@@ -0,0 +1,15 @@
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+paragraph.innerText = "Vou falar o meu filme favorito abaixo:";
+
+const title = document.getElementById("page-title");
+title.innerText = 'Meu Filme Favorito';
+title.style.color = 'green';
+
+const subtitle = document.getElementById("subtitle");
+subtitle.innerText = 'Trilogia - O Senhor dos Anéis.'
+subtitle.style.fontFamily = 'Arial';
+subtitle.style.fontSize = '25px';
+
+const secondP = document.getElementById("second-paragraph");
+secondP.innerText = 'A melhor trilogia de todas.';
