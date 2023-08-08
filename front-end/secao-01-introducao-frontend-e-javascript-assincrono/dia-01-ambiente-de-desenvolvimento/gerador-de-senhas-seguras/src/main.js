61dfd59d8808450f85c4abb371c9c18219012fdb
fernando-arcoverde <nando.arcoverde@gmail.com>
2023-08-08 09:46:22 -0300

    instalando e configurando o eslint no projeto

--- front-end/secao-01-introducao-frontend-e-javascript-assincrono/dia-01-ambiente-de-desenvolvimento/gerador-de-senhas-seguras/src/main.js
+++ front-end/secao-01-introducao-frontend-e-javascript-assincrono/dia-01-ambiente-de-desenvolvimento/gerador-de-senhas-seguras/src/main.js
@@ -1,7 +1,7 @@
-import { nanoid } from "nanoid";
+import { nanoid } from 'nanoid';
 
 const passwodButtonElement = document.getElementById('passwordBtn');
-const displayPasswordElement = document.querySelector("h2");
+const displayPasswordElement = document.querySelector('h2');
 
 passwodButtonElement.addEventListener('click', () => {
     const randomPassword = nanoid();
@@ -15,4 +15,5 @@ passwodButtonElement.addEventListener('click', () => {
 1- Adicionado a chave "type": "module" no package.json para roda o nanoid no node através do comando no terminal node src/main.js .
 2- Utilizado o comando npx vite ou vite --open.
 3- Vite instalado com o comando npm install vite e adicionado uma chave no package.json "dev": "vite --open"
+4- Utilizado o comando npm init @eslint/config.
 */
