19124385ec5310de56b9760a2186798911f2e2b3
fernando-arcoverde <nando.arcoverde@gmail.com>
2023-08-08 08:31:23 -0300

    capturando elementos com DOM

--- front-end/secao-01-introducao-frontend-e-javascript-assincrono/dia-01-ambiente-de-desenvolvimento/gerador-de-senhas-seguras/src/main.js
+++ front-end/secao-01-introducao-frontend-e-javascript-assincrono/dia-01-ambiente-de-desenvolvimento/gerador-de-senhas-seguras/src/main.js
@@ -1,9 +1,16 @@
 import { nanoid } from "nanoid";
 
-const randomPassword = nanoid();
-console.log(randomPassword);
+const passwodButtonElement = document.getElementById('passwordBtn');
+const displayPasswordElement = document.querySelector("h2");
 
+passwodButtonElement.addEventListener('click', () => {
+    console.log('Testando Click');
+});
+
+// const randomPassword = nanoid();
+// console.log(randomPassword);
 /*
 [] Observações:
 1- Adicionado a chave "type": "module" no package.json para roda o nanoid no node através do comando no terminal node src/main.js .
+2- Utilizado o comando npx vite ou vite --open.
 */
