0d4f0d2f5bcc4e4ef43c43c71868c86be872459d
fernando-arcoverde <nando.arcoverde@gmail.com>
2023-08-08 08:20:21 -0300

    utilizando vite

--- front-end/secao-01-introducao-frontend-e-javascript-assincrono/dia-01-ambiente-de-desenvolvimento/gerador-de-senhas-seguras/src/main.js
+++ front-end/secao-01-introducao-frontend-e-javascript-assincrono/dia-01-ambiente-de-desenvolvimento/gerador-de-senhas-seguras/src/main.js
@@ -1,1 +1,9 @@
-console.log('Hello world');
+import { nanoid } from "nanoid";
+
+const randomPassword = nanoid();
+console.log(randomPassword);
+
+/*
+[] Observações:
+1- Adicionado a chave "type": "module" no package.json para roda o nanoid no node através do comando no terminal node src/main.js .
+*/
