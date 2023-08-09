e048765f772c4f9acc4f2b00fa112bbf7f7a7ea5
fernando-arcoverde <nando.arcoverde@gmail.com>
2023-08-09 08:05:13 -0300

    Clipboard Copy instalada e funcional.

--- front-end/secao-01-introducao-frontend-e-javascript-assincrono/dia-01-ambiente-de-desenvolvimento/gerador-de-senhas/src/main.js
+++ front-end/secao-01-introducao-frontend-e-javascript-assincrono/dia-01-ambiente-de-desenvolvimento/gerador-de-senhas/src/main.js
@@ -1,5 +1,6 @@
 import { nanoid } from 'nanoid';
 import './style.css';
+import copy from 'clipboard-copy';
 
 const passwordBtEl = document.querySelector('button');
 const displayPassworEl = document.querySelector('h2');
@@ -7,4 +8,9 @@ const displayPassworEl = document.querySelector('h2');
 passwordBtEl.addEventListener('click', () => {
     const randomPassword = nanoid();
     displayPassworEl.innerHTML = randomPassword;
+});
+
+displayPassworEl.addEventListener('click', (event) => {
+    copy(event.target.innerHTML);
+    alert('Senha copiada!');
 });
