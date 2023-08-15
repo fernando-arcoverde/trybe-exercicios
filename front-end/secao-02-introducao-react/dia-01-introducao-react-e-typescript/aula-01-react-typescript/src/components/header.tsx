edfb663587bc370c76cdbdd6c8da3589939fa41b
fernando-arcoverde <nando.arcoverde@gmail.com>
2023-08-15 13:27:27 -0300

    Jogos aleatórios da mega-sena com React

--- front-end/secao-02-introducao-react/dia-01-introducao-react-e-typescript/aula-01-react-typescript/src/components/header.tsx
+++ front-end/secao-02-introducao-react/dia-01-introducao-react-e-typescript/aula-01-react-typescript/src/components/header.tsx
@@ -1,10 +1,30 @@
-const person = {
-    name: 'John',
-    lastname: 'Wick',
-}
-
 function Header() {
-    return <h1>{`Olá, ${person.name} ${person.lastname}.`}</h1>
+    const person = {
+        name: 'John',
+        lastname: 'Wick',
+    }
+
+    function luckyNumber() {
+        return Math.floor(Math.random() * 60 + 1);
+    }
+
+    return (
+        <div>
+            <h1 className='greeting'>
+                {`Olá, ${person.name} ${person.lastname}`}
+                <img src="https://i.imgur.com/rqvLd3q.png" alt="" />
+            </h1>
+            <h2 className='subtitle'>Seus números da sorte da Mega-Sena são:</h2>
+            <ul>
+                <li>{luckyNumber()}</li>
+                <li>{luckyNumber()}</li>
+                <li>{luckyNumber()}</li>
+                <li>{luckyNumber()}</li>
+                <li>{luckyNumber()}</li>
+                <li>{luckyNumber()}</li>
+            </ul>
+        </div>
+    )
 }
 
 export default Header;
