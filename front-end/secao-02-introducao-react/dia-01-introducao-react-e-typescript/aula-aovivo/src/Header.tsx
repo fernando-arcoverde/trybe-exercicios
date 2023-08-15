8e5207ac6e6d4ef609d35a46a6bb481abe9216dc
fernando-arcoverde <nando.arcoverde@gmail.com>
2023-08-15 15:30:44 -0300

    Trabalhando com exemplos da aula ao vivo.

---
+++ front-end/secao-02-introducao-react/dia-01-introducao-react-e-typescript/aula-aovivo/src/Header.tsx
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <header>
+            <img src="https://avatars.githubusercontent.com/u/125072921?v=4" alt="" />
+            <h1>Fernando Arcoverde</h1>
+            <h2>Front-end</h2>
+        </header>
+    );
+}
+
+export default Header;
