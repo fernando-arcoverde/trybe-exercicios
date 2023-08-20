dc0952dbc4130887d4aeadf9f72d09ebe2ca067b
fernando-arcoverde <nando.arcoverde@gmail.com>
2023-08-20 15:59:18 -0300

    Trabalhando com objeto e template literals dentro do componente

--- front-end/secao-02-introducao-react/dia-01-introducao-react-e-typescript/aula-aovivo/src/components/Header.tsx
+++ front-end/secao-02-introducao-react/dia-01-introducao-react-e-typescript/aula-aovivo/src/components/Header.tsx
@@ -1,10 +1,18 @@
 function Header() {
+  const cardInfo = {
+    name: 'Fernando',
+    lastname: 'Arcoverde',
+    age: 38,
+    role: 'Frontend',
+    phrase: 'Você é aquilo que vibra',
+  };
+
   return (
     <header>
       <img src="https://avatars.githubusercontent.com/u/125072921?s=400&u=2f65c57391c743b2d239e7963cdad5dab9e4a66d&v=4" alt="" />
-      <h1>Fernando Arcoverde</h1>
-      <h2>Frontend</h2>
-      <h3>Você é aquilo que vibra!</h3>
+      <h1>{`${cardInfo.name} ${cardInfo.lastname}, ${cardInfo.age}`}</h1>
+      <h2>{`${cardInfo.role}`}</h2>
+      <h3>{`${cardInfo.phrase}`}</h3>
     </header>
   );
 }
