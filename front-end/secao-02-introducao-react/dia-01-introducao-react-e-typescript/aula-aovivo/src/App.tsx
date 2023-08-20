0c7c74e91174626cd83cad2c3981091ff2bf4266
fernando-arcoverde <nando.arcoverde@gmail.com>
2023-08-20 15:27:06 -0300

    Iniciando projeto aula ao-vivo

---
+++ front-end/secao-02-introducao-react/dia-01-introducao-react-e-typescript/aula-aovivo/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hello World!</h1>
+      <img src="https://avatars.githubusercontent.com/u/125072921?s=400&u=2f65c57391c743b2d239e7963cdad5dab9e4a66d&v=4" alt="" />
+    </div>
+  );
+}
+
+export default App;
