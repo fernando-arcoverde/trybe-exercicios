1dd95bfd63b090e66ec04486e642e378930a7297
fernando-arcoverde <nando.arcoverde@gmail.com>
2023-08-20 15:48:13 -0300

    Criando primeiro componente

---
+++ front-end/secao-02-introducao-react/dia-01-introducao-react-e-typescript/aula-aovivo/src/components/Header.tsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <header>
+      <img src="https://avatars.githubusercontent.com/u/125072921?s=400&u=2f65c57391c743b2d239e7963cdad5dab9e4a66d&v=4" alt="" />
+      <h1>Fernando Arcoverde</h1>
+      <h2>Frontend</h2>
+      <h3>Você é aquilo que vibra!</h3>
+    </header>
+  );
+}
+
+export default Header;
