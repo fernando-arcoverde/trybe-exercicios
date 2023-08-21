7baeb55760bd616d7a41aff381c0096fcd3b03ff
fernando-arcoverde <nando.arcoverde@gmail.com>
2023-08-21 07:16:41 -0300

    Requisito 5 - Exercício para fixação de componentes React finalizado.

--- front-end/secao-02-introducao-react/dia-01-introducao-react-e-typescript/exercicios/src/components/HelloWorld.tsx
+++ front-end/secao-02-introducao-react/dia-01-introducao-react-e-typescript/exercicios/src/components/HelloWorld.tsx
@@ -1,9 +1,16 @@
 import Title from './Title';
 import ModulesDetails from './ModulesDetails';
+import { calculateAge } from './utils/age';
 
 function HelloWorld() {
+  const name = 'Fernando Arcoverde';
+  const birth = '20/04/1985';
+  const age = calculateAge(birth);
+
   return (
     <>
+      <h1 style={ { color: 'green', textAlign: 'center' } }>{name}</h1>
+      <h2>{`Idade, ${age}.`}</h2>
       <Title />
       <ModulesDetails />
     </>
