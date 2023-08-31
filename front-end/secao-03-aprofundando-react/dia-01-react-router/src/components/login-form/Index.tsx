fa6ac4343ad33198e85fd3d24cf68f9402a01001
fernando-arcoverde <nando.arcoverde@gmail.com>
2023-08-31 20:04:57 -0300

    Index Login Form finalizado.

---
+++ front-end/secao-03-aprofundando-react/dia-01-react-router/src/components/login-form/Index.tsx
@@ -0,0 +1,74 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import './login-form.css';
+
+function LoginForm() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(false);
+
+  const navigate = useNavigate();
+
+  function handleEmail(event: React.ChangeEvent<HTMLInputElement>) {
+    const { target } = event;
+    setEmail(target.value);
+  }
+
+  function handlePassword(event: React.ChangeEvent<HTMLInputElement>) {
+    const { target } = event;
+    setPassword(target.value);
+  }
+
+  function validateLogin() {
+    const regex = /\S+@\S+\.\S+/i;
+    if (regex.test(email) && password.length > 5) return true;
+    return false;
+  }
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const validate = validateLogin();
+
+    if (validate) {
+      navigate('/tasks');
+    } else {
+      setError(true);
+    }
+  }
+
+  return (
+    <form onSubmit={ handleSubmit }>
+      <div className="inputs-container">
+        <label htmlFor="login">
+          E-mail
+          <input
+            id="login"
+            value={ email }
+            onChange={ handleEmail }
+            required
+            placeholder="carol@trybe.com"
+          />
+        </label>
+        <label htmlFor="password">
+          Senha
+          <input
+            value={ password }
+            onChange={ handlePassword }
+            type="password"
+            required
+          />
+        </label>
+        {error && (
+          <div className="error-msg">
+            <p>E-mail inválido ou senha com menos de 6 caracteres</p>
+          </div>
+        )}
+      </div>
+      <button>Enviar</button>
+    </form>
+  );
+}
+
+export default LoginForm;
